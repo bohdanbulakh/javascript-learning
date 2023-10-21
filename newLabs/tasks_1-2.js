6cb189051e157e16bdee47047f9aa88b2e8a463d
bohdan <bgdn.56t@gmail.com>
2023-10-22 00:20:09 +0300

    task 2 solution

--- newLabs/tasks_1-2.js
+++ newLabs/tasks_1-2.js
@@ -8,3 +8,20 @@ const items = [
 for (const item of items){
     console.log(`Price: ${item.price}`);
 }
+
+function getTotalPrice(goods) {
+    let total = 0;
+
+    for (const { price } of goods) {
+        if (typeof price === 'number' && price >= 0) {
+            total += price;
+        } else {
+            throw Error(`${price} isn't a number or it is negative`)
+        }
+    }
+
+    return total;
+}
+
+const totalPrice = getTotalPrice(items);
+console.log(`Total price of items is ${totalPrice}`);
