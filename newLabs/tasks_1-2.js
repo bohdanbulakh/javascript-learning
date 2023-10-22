00334c3c21d6f35cd1917194a87feff3f7970f55
bohdan <bgdn.56t@gmail.com>
2023-10-22 17:10:22 +0300

    code style fixed with eslint

--- newLabs/tasks_1-2.js
+++ newLabs/tasks_1-2.js
@@ -1,26 +1,28 @@
+'use strict';
+
 const items = [
-    { price: 40 },
-    { price: 120 },
-    { price: 505 },
-    { price: 350 },
+  { price: 40 },
+  { price: 120 },
+  { price: 505 },
+  { price: 350 },
 ];
 
-for (const item of items){
-    console.log(`Price: ${item.price}`);
+for (const item of items) {
+  console.log(`Price: ${item.price}`);
 }
 
 function getTotalPrice(goods) {
-    let total = 0;
+  let total = 0;
 
-    for (const { price } of goods) {
-        if (typeof price === 'number' && price >= 0) {
-            total += price;
-        } else {
-            throw Error(`${price} isn't a number or it is negative`)
-        }
+  for (const { price } of goods) {
+    if (typeof price === 'number' && price >= 0) {
+      total += price;
+    } else {
+      throw Error(`${price} isn't a number or it is negative`);
     }
+  }
 
-    return total;
+  return total;
 }
 
 const totalPrice = getTotalPrice(items);
--- newLabs/tasks_3-5.js
+++ newLabs/tasks_3-5.js
@@ -1,44 +1,44 @@
-'use strict'
+'use strict';
 
 const purchase = {
-    Electronics: [
-        { name: 'Laptop', price: 1500 },
-        { name: 'Keyboard', price: 100 },
-        { name: 'Flash drive', price: 70 },
-        { name: 'Smartphone', price: 1200 },
-    ],
-    Textile: [
-        { name: 'Bag', price: 50 },
-        { name: 'T-shirt', price: 100 },
-    ],
-    Food: [
-        { name: 'Bananas', price: 10 },
-        { name: 'Apples', price: 15 },
-        { name: 'Juice', price: 20 },
-        { name: 'Sausages', price: 40 },
-        { name: 'Bananas', price: 15 },
-        { name: 'Cheese', price: 35 },
-    ],
-    Furniture: [
-        { name: 'Table', price: 1000 },
-        { name: 'Chair', price: 500 },
-    ]
+  Electronics: [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Keyboard', price: 100 },
+    { name: 'Flash drive', price: 70 },
+    { name: 'Smartphone', price: 1200 },
+  ],
+  Textile: [
+    { name: 'Bag', price: 50 },
+    { name: 'T-shirt', price: 100 },
+  ],
+  Food: [
+    { name: 'Bananas', price: 10 },
+    { name: 'Apples', price: 15 },
+    { name: 'Juice', price: 20 },
+    { name: 'Sausages', price: 40 },
+    { name: 'Bananas', price: 15 },
+    { name: 'Cheese', price: 35 },
+  ],
+  Furniture: [
+    { name: 'Table', price: 1000 },
+    { name: 'Chair', price: 500 },
+  ]
 };
 
 
 const find = (goods, name) => {
-    const result = [];
+  const result = [];
 
-    for (const goodsGroup in goods) {
-        for (const product of goods[goodsGroup]) {
-            if (product.name === name) {
-                result.push(product);
-            }
-        }
+  for (const goodsGroup in goods) {
+    for (const product of goods[goodsGroup]) {
+      if (product.name === name) {
+        result.push(product);
+      }
     }
+  }
 
-    return result;
-}
+  return result;
+};
 
 const result = find(purchase, 'Bananas');
 console.log(result);
