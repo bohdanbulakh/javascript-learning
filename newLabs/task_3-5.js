e95ab1715d3e91110201d7a71ef48740ced7372e
bohdan <bgdn.56t@gmail.com>
2023-10-22 14:38:56 +0300

    task 3 solution

---
+++ newLabs/task_3-5.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const purchase = {
+    Electronics: [
+        { name: 'Laptop', price: 1500 },
+        { name: 'Keyboard', price: 100 },
+        { name: 'Flash drive', price: 70 },
+        { name: 'Smartphone', price: 1200 },
+    ],
+    Textile: [
+        { name: 'Bag', price: 50 },
+        { name: 'T-shirt', price: 100 },
+    ],
+    Food: [
+        { name: 'Bananas', price: 10 },
+        { name: 'Apples', price: 15 },
+        { name: 'Juice', price: 20 },
+        { name: 'Sausages', price: 40 },
+        { name: 'Cheese', price: 35 },
+    ],
+    Furniture: [
+        { name: 'Table', price: 1000 },
+        { name: 'Chair', price: 500 },
+    ]
+};
+
+console.log(purchase);
